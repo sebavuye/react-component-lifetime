508daa07d21cd75e67454b39bda9ec5345bde3a2
sebastianvuye <sebastian.vuye@gmail.com>
2023-04-12 13:34:39 +0200

    feat: add italic and bold mdx component

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -4,10 +4,12 @@ import type { ReactNode } from "react";
 
 export const mdxComponents = {
   code: (props: { children: ReactNode }) => <code className="hljs" {...props} />,
+  em: (props: { children: ReactNode }) => <span className="italic" {...props} />,
   h1: (props: HeadingProps) => <Heading level="h1" {...props} />,
   h2: (props: HeadingProps) => <Heading level="h2" {...props} />,
   h3: (props: HeadingProps) => <Heading level="h3" {...props} />,
   h4: (props: HeadingProps) => <Heading level="h4" {...props} />,
   h5: (props: HeadingProps) => <Heading level="h5" {...props} />,
   h6: (props: HeadingProps) => <Heading level="h6" {...props} />,
+  strong: (props: { children: ReactNode }) => <span className="font-bold" {...props} />,
 };
